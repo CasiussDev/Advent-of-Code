55a4ebc932c6f5b39ad088baa9e914590ad6cbc3
CasiussDev <slugserjee@gmail.com>
2022-11-29 22:28:10 +0000

    day 1/2022 part 1

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+extern crate itertools;
+
+use itertools::Itertools;
+use std::io;
+use std::io::BufRead;
+use std::fs;
+
+fn main() -> io::Result<()> {
+    let file = fs::File::open("input.txt")?;
+    let file_reader = io::BufReader::new(file);
+
+    let increase_count = file_reader
+        .lines()
+        .filter_map(|line|line.ok())
+        .filter_map(|line| line.parse::<i64>().ok())
+        .tuple_windows()
+        .filter(|(lhs, rhs)| lhs < rhs)
+        .count();
+    println!("{increase_count}");
+
+    Ok(())
 }
