a14b59ea3ce1127e0a386fc3ae256f235cb00a69
casiuss <slugserjee@gmail.com>
2022-11-30 23:33:22 +0100

    day1/2021 part 2

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,22 +1,40 @@
 extern crate itertools;
 
 use itertools::Itertools;
+use std::fs;
 use std::io;
 use std::io::BufRead;
-use std::fs;
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = fs::File::open("input.txt")?;
     let file_reader = io::BufReader::new(file);
 
-    let increase_count = file_reader
+    let samples = file_reader
         .lines()
-        .filter_map(|line|line.ok())
-        .filter_map(|line| line.parse::<i64>().ok())
+        .map(|line| line.map_err(|error| Box::<dyn std::error::Error>::from(error)))
+        .map(|line| {
+            line.and_then(|line| {
+                line.parse::<i64>()
+                    .map_err(|error| Box::<dyn std::error::Error>::from(error))
+            })
+        })
+        .collect::<Result<Vec<i64>, Box<dyn std::error::Error>>>()?;
+
+    let increase_depth_count = samples
+        .iter()
         .tuple_windows()
-        .filter(|(lhs, rhs)| lhs < rhs)
+        .filter(|(lhs, rhs)| rhs > lhs)
         .count();
-    println!("{increase_count}");
+    println!("{increase_depth_count} samples are greater than the previous one");
+
+    let increase_depth_sum_count = samples
+        .iter()
+        .tuple_windows()
+        .map(|(x, y, z)| x + y + z)
+        .tuple_windows()
+        .filter(|(lhs, rhs)| rhs > lhs)
+        .count();
+    println!("{increase_depth_sum_count} 3-sample sums are greater than the previous one");
 
     Ok(())
 }
