ae14af961ebbc6cc027bcf5d787774a42069416c
casiuss <slugserjee@gmail.com>
2022-12-06 11:34:06 +0100

    day6**/2022  add find_start_sequence generic func

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -1,10 +1,23 @@
 extern crate itertools;
 
+use crate::start_bytes_window::{StartBytesWindow, StartOfMessage, StartOfPacket};
 use io::Read;
 use std::{fs, io};
-use crate::start_bytes_window::{StartOfPacket, StartOfMessage};
 
 mod start_bytes_window;
+
+fn find_start_sequence<I, const N: usize>(
+    it: &mut I,
+    mut window: StartBytesWindow<N>,
+) -> Option<(usize, u8)>
+where
+    I: Iterator<Item = (usize, u8)>,
+{
+    it.find(|(_, b)| {
+        window.push(*b);
+        window.is_starting_sequence()
+    })
+}
 
 fn main() {
     let input_file = fs::File::open("input.txt").unwrap();
@@ -12,33 +25,28 @@ fn main() {
 
     let mut window = StartOfPacket::new();
 
-    let mut it = input_reader
-        .bytes()
-        .map(|b| b.unwrap())
-        .enumerate();
+    let mut it = input_reader.bytes().map(|b| b.unwrap()).enumerate();
 
-    let result = it
-        .find(|(_, b)| {
-            window.push(*b);
-            window.is_starting_sequence()
-        });
+    let result = find_start_sequence(&mut it, window);
 
     if let Some((index, _)) = result {
-        println!("Start of packet appeared after processing {} chars", index + 1);
+        println!(
+            "Start of packet appeared after processing {} chars",
+            index + 1
+        );
     } else {
         println!("No start of packet found");
     }
 
     let mut window = StartOfMessage::new();
 
-    let result = it
-        .find(|(_, b)| {
-            window.push(*b);
-            window.is_starting_sequence()
-        });
+    let result = find_start_sequence(&mut it, window);
 
     if let Some((index, _)) = result {
-        println!("Start of message appeared after processing {} chars", index + 1);
+        println!(
+            "Start of message appeared after processing {} chars",
+            index + 1
+        );
     } else {
         println!("No start of message found");
     }
